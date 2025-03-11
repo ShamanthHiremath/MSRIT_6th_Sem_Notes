08f394f5e2152f8bc23b1d644637d51628890f3c
Shamanth M Hiremath <123861244+ShamanthHiremath@users.noreply.github.com>
2025-03-11 06:13:14 +0000

    avsahsvah

---
+++ USP/week2/p3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char buffer[100]; // Define a buffer to store the input
+
+    printf("Enter some text: ");
+
+    // Read a line of text from standard input
+    fgets(buffer, sizeof(buffer), stdin);
+
+    // Display the text entered by the user
+    printf("You entered: %s", buffer);
+
+    return 0;
+}
